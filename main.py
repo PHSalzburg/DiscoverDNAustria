d5575dc230a2a613d362371450b4cc1aaad4ebcc
PHSalzburg <152970035+PHSalzburg@users.noreply.github.com>
2023-12-05 19:24:40 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+
+# Example code to read a Google Sheets document into a Pandas DataFrame
+url = 'https://docs.google.com/spreadsheets/d/1e0b0mybZrToEL6QYPj-_i7aKUBrP0eGHvOQozO_Wo7A/edit#gid=1080009798'
+csv_export_url = url.replace('/edit#gid=', '/export?format=csv&gid=')
+df = pd.read_csv(csv_export_url)
+
+df_json = {"events":[]}
+
+for i in range(len(df)):
+    df_json["events"].append(df.iloc[i].to_dict())
+    
+json.dumps(df_json, indent=3)
